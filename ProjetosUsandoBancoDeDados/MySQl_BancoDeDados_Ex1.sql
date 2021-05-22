263716ff224d3d97b1012ca6a471b93b1b4812fc
Kevin Kurt <kevin-kurt@bol.com.br>
2021-05-21 21:28:00 -0300

    Update MySQl_BancoDeDados_Ex1.sql

--- ProjetosUsandoBancoDeDados/MySQl_BancoDeDados_Ex1.sql
+++ ProjetosUsandoBancoDeDados/MySQl_BancoDeDados_Ex1.sql
@@ -37,7 +37,7 @@ select * from tb_classe;
 
 select * from tb_personagem where ataque > 2000;
 
-select * from tb_personagem where defesa between 1000 and 2000;
+select * from tb_personagem inner join tb_classe on tb_classe.id = tb_personagem.classe_id where defesa between 1000 and 2000;
 
 select * from tb_personagem where nome like "%C%";
 
